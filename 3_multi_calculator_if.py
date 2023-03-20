b25e72aa22d4a4ae5253e14d356ba34713b689ae
brian <bkim@thu>
2023-03-20 09:15:23 +0800

    Standard deviation

--- 3_multi_calculator_if.py
+++ 3_multi_calculator_if.py
@@ -7,6 +7,7 @@ sum = 0
 # 1. Input
 x1 = input("Enter x1: ")
 x2 = input("Enter x2: ")
+x3 = input("Enter x3: ")
 op = input("Enter operator: ")
 
 # 2. Process
@@ -23,11 +24,14 @@ elif op == "avg":
 elif op == "sd":
 #
 #   Homework 3
-#     
+#   Input: x1=5, x2=10, x3=11
+#   Output: SD=xx.xxx
+#
 1. calculate mean
-2. tmp1 = mean - x1
-3. tmp2 = mean - x2
-4. (tmp1+ tmp2)**2
+2. tmp1 = x1 - mean
+3. tmp2 = x2 - mean
+3. tmp3 = x3 - mean
+4. (tmp1 + tmp2 + tmp3)**2
 5. #4/n
 5. sqrt(#5)
 
