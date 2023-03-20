5a63a44e0d30b2a36706bfef618453ac3a4f5b38
brian <bkim@thu>
2023-03-20 09:05:25 +0800

    Assignment 3

--- 3_multi_calculator_if.py
+++ 3_multi_calculator_if.py
@@ -14,6 +14,22 @@ if op == "+":
     sum = int(x1) + int(x2)
 elif op == "-":
     sum = int(x1) - int(x2)
+elif op == "*":
+    sum = int(x1) * int(x2)
+elif op == "/":
+    sum = int(x1) / int(x2)
+elif op == "avg":
+    sum = (int(x1) + int(x2))/2
+elif op == "sd":
+#
+#   Homework 3
+#     
+1. calculate mean
+2. tmp1 = mean - x1
+3. tmp2 = mean - x2
+4. (tmp1+ tmp2)**2
+5. #4/n
+5. sqrt(#5)
 
 # 3. Output
 print(f"Sum: {sum}")
