51f80852cbbb15554ed431d157641315729edde2
thugmba <125806659+thugmba@users.noreply.github.com>
2023-04-16 23:20:31 +0800

    Add files via upload
    function

---
+++ 13_function_2.py
@@ -0,0 +1,25 @@
+#
+# Template for Python program
+#
+import statistics, math
+
+def my_sd(input):
+    print(f'Input : {input}')
+    length = len(input)
+    sum = 0
+    mean = statistics.mean(input)
+    print(f'Mean  : {mean}')
+    for x in input:
+        sum += (int(x) - mean)**2
+    sum = sum / length
+    output = math.sqrt(sum)    
+    return output
+
+# 1. Input
+input = [10,20,30]
+
+# 2. Process
+answer = my_sd(input)
+
+# 3. Output
+print(f'Answer: {answer}')
